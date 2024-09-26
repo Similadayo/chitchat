7d9147e9afe02976d3c0b67f319fdd7e95c9ecad
similadayo <similoluwailesanmi@outlook.com>
2024-09-26 10:52:56 +0100

    Refactor database configuration and environment loading

--- config/database.go
+++ config/database.go
@@ -2,25 +2,15 @@ package config
 
 import (
 	"fmt"
-	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-// LoadEnvironment loads environment variables from the .env file
-func LoadEnvironment() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-}
-
 // ConnectDB establishes a connection to the database
 func ConnectDB() (*gorm.DB, error) {
-	LoadEnvironment()
+	LoadEnv()
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
