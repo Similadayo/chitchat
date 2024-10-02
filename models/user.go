fe5dcee3e7320788463332c8a338a20404870897
similadayo <similoluwailesanmi@outlook.com>
2024-10-02 18:43:11 +0100

    Add profile picture field to User model

--- models/user.go
+++ models/user.go
@@ -6,6 +6,7 @@ import (
 	"gorm.io/gorm"
 )
 
+// User model with image field
 type User struct {
 	gorm.Model
 	Username    string    `json:"username" gorm:"unique;not null"`
@@ -14,6 +15,7 @@ type User struct {
 	FirstName   string    `json:"first_name"`
 	LastName    string    `json:"last_name"`
 	DateOfBirth time.Time `json:"date_of_birth"`
+	ProfilePic  string    `json:"profile_pic"`
 	IsActive    bool      `json:"is_active" gorm:"default:true"`
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
