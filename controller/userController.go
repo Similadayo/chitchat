8d75ff6d22e668d71b086d464795f5df7558569c
similadayo <similoluwailesanmi@outlook.com>
2024-10-02 18:46:31 +0100

    Add user controller and registration endpoint

---
+++ controller/userController.go
@@ -0,0 +1,63 @@
+package controller
+
+import (
+	"encoding/json"
+	"github/similadayo/chitchat/config"
+	"github/similadayo/chitchat/models"
+	"github/similadayo/chitchat/utils"
+	"net/http"
+
+	"gorm.io/gorm"
+)
+
+type UserController struct {
+	DB *gorm.DB
+}
+
+// NewUserController creates a new user controller
+func NewUserController() *UserController {
+	db, err := config.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	config.MigrateDB(db)
+	return &UserController{DB: db}
+}
+
+// RegisterUser registers a new user
+func (uc *UserController) RegisterUser(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+
+	//Parse the request body
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	//Hash the password before saving the user
+	hashedPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		http.Error(w, "Could not hash the password", http.StatusInternalServerError)
+		return
+	}
+	user.Password = hashedPassword
+
+	//handles image upload for profile picture
+	file, handler, err := r.FormFile("profile_pic")
+	if err == nil {
+		defer file.Close()
+		user.ProfilePic = handler.Filename
+	}
+
+	//Save the user in the database
+	if err := uc.DB.Create(&user).Error; err != nil {
+		http.Error(w, "Could not save the user", http.StatusInternalServerError)
+		return
+	}
+
+	//Respond with success message
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "User created successfully"})
+}
