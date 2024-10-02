bac091dc3892c3218b88c71d83c013ef9cf909de
similadayo <similoluwailesanmi@outlook.com>
2024-10-02 18:58:19 +0100

    Refactor routes.go and add user registration and login endpoints

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,23 @@
 package routes
 
-import "github.com/gorilla/mux"
+import (
+	"github/similadayo/chitchat/controller"
+
+	"github.com/gorilla/mux"
+)
 
 func InitRoutes() *mux.Router {
 	router := mux.NewRouter()
 
+	// Creates new user controller
+	userController := controller.NewUserController()
+
+	// Add routes here
+	router.HandleFunc("/register", userController.RegisterUser).Methods("POST")
+	router.HandleFunc("/login", userController.LoginUser).Methods("POST")
+
+	// Add routes for the user profile
+
 	return router
+
 }
